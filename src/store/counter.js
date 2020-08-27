d161d40e021517a2656ddcf3b34fa4d1c82ed21d
duanguang <duanguang@hoolinks.com>
2020-08-27 18:28:56 +0800

    feat: 升级小程序版本

--- src/store/counter.js
+++ src/store/counter.js
@@ -1,45 +1,33 @@
-import { observable,action,autorun,computed } from 'mobx';
-import { getPalletService } from '../services/search';
-import {observablePromise} from 'hoolinks/mobx'
-export default class counterStore{
-    constructor(){
-      this.watch();
-    }
-    @computed get palletList(){
-        if(this.pallet.isResolved){
-            let rows=this.pallet.value.result
-            return rows.map((item,index)=>{
-                return {key:`${item.productName}${index}`,name:item.productName}
-              })
-        }
-        return [];
-    }
-    watch=()=>{
-        autorun(()=> {
-          // console.log(this.pallet,this.pallet.state)
-         });
-    }
+import {
+  observable,
+  action,
+  autorun,
+} from 'mobx';
 
-    @observable  counter=0;
-    @observable pallet=observablePromise(null);
-    @action
-    increment(){
-        this.counter++
-    }
+export default class counterStore {
+  constructor() {
+    this.watch();
+  }
+  watch = () => {
+    autorun(() => {
+      // console.log(this.pallet,this.pallet.state)
+    });
+  }
 
-    @action
-    decrement(){
-        this.counter--
-    }
-    @action
-    incrementAsync(){
-        setTimeout(() => {
-            this.counter++
-          }, 1000);
-    }
+  @observable counter = 0;
+  @action
+  increment() {
+    this.counter++
+  }
 
-    @action
-    getPallet(){
-         this.pallet=observablePromise(getPalletService())
-    }
+  @action
+  decrement() {
+    this.counter--
+  }
+  @action
+  incrementAsync() {
+    setTimeout(() => {
+      this.counter++
+    }, 1000);
+  }
 }
