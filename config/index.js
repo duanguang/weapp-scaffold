8df9fb1c913b17a67aaf362d9cf7389ce325cc00
duanguang <duanguang@hoolinks.com>
2020-08-28 15:32:26 +0800

    feat: 增加包管理大小分析插件

--- config/index.js
+++ config/index.js
@@ -1,74 +1,75 @@
 const config = {
-  projectName: 'cloudHub-Wechat',
-  date: '2018-12-6',
+  projectName: 'project-demo-ts',
+  date: '2020-8-27',
   designWidth: 750,
   deviceRatio: {
-    '640': 2.34 / 2,
-    '750': 1,
-    '828': 1.81 / 2
+    640: 2.34 / 2,
+    750: 1,
+    828: 1.81 / 2,
   },
   sourceRoot: 'src',
   outputRoot: 'dist',
-  plugins: {
-    babel: {
-      sourceMap: true,
-      presets: [
-        'env'
-      ],
-      plugins: [
-        'transform-decorators-legacy',
-        'transform-class-properties',
-        'transform-object-rest-spread'
-      ]
-    }
-  },
-  defineConstants: {
-  },
+  plugins: [],
+  defineConstants: {},
   copy: {
-     patterns: [
-    ],
-    options: {
-    }
+    patterns: [],
+    options: {},
   },
-  weapp: {
-    module: {
-      postcss: {
-        autoprefixer: {
-          enable: true
-        },
-        url: {
-          enable: true,
-          config: {
-            limit: 10240 // 设定转换尺寸上限
-          }
-        }
-      }
+  terser: {
+    enable: true,
+    config: {
+      // 配置项同 https://github.com/terser/terser#minify-options
     },
-    compile: {
-      exclude: ['hoolinks']
-    }
+  },
+  framework: 'react',
+  mini: {
+    postcss: {
+      pxtransform: {
+        enable: true,
+        config: {},
+      },
+      url: {
+        enable: true,
+        config: {
+          limit: 1024, // 设定转换尺寸上限
+        },
+      },
+      cssModules: {
+        enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
+        config: {
+          namingPattern: 'module', // 转换模式，取值为 global/module
+          generateScopedName: '[name]__[local]___[hash:base64:5]',
+        },
+      },
+    },
+    webpackChain(chain, webpack) {
+      chain
+        .plugin('analyzer')
+        .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin, []);
+    },
   },
   h5: {
     publicPath: '/',
     staticDirectory: 'static',
-    module: {
-      postcss: {
-        autoprefixer: {
-          enable: true
-        }
-      }
-    }
-  }
-}
+    postcss: {
+      autoprefixer: {
+        enable: true,
+        config: {},
+      },
+      cssModules: {
+        enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
+        config: {
+          namingPattern: 'module', // 转换模式，取值为 global/module
+          generateScopedName: '[name]__[local]___[hash:base64:5]',
+        },
+      },
+    },
+  },
+};
 
 module.exports = function (merge) {
   if (process.env.NODE_ENV === 'development') {
-    if (process.env.TARO_ENV === "weapp") {
-      require("taro-ui/dist/weapp/css/index.css")
-    } else if (process.env.TARO_ENV === "h5") {
-      require("taro-ui/dist/h5/css/index.css")
-    }
-    return merge({}, config, require('./dev'))
+    return merge({}, config, require('./dev'));
   }
-  return merge({}, config, require('./prod'))
-}
+  return merge({}, config, require('./prod'));
+};
